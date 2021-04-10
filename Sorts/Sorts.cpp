bb5696751468842868ce8c0c190ae97bd8957756
Kojiemista <58912484+Kojiemista@users.noreply.github.com>
2021-04-10 23:57:52 +0300

    1

--- Sorts/Sorts.cpp
+++ Sorts/Sorts.cpp
@@ -1,7 +1,17 @@
 ﻿#include <vector>
 #include <ctime>
+using namespace std;
+
+void fillVector(vector <int>& tmpVector, const int &size)
+{
+	for (int i = 0; i < size; ++i) tmpVector.push_back(rand() % 100);
+}
 
 int main()
 {
-    
+	srand(time(NULL));
+	const int size = 10000;
+	vector <int> tmpVector;
+	fillVector(tmpVector, size);
+	return 0;
 }

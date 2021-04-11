64280d1615e914b03181ac11724ab7a992e24193
Kojiemista <58912484+Kojiemista@users.noreply.github.com>
2021-04-11 11:43:05 +0300

    1

--- Sorts/Sorts.cpp
+++ Sorts/Sorts.cpp
@@ -2,6 +2,22 @@
 #include <ctime>
 using namespace std;
 
+void sortInsert(vector <int>& tmpVector) //Сортировка вставками, эффективен при малом количестве элементов
+{
+	for (size_t i = 1; i < tmpVector.size(); ++i)
+	{
+		for (int b = i; b > 0 && tmpVector.at(b) < tmpVector.at(b - 1); --b)
+		{
+			swap(tmpVector.at(b), tmpVector.at(b - 1));
+		}
+	}
+}
+
+void sortMerge(vector <int>& tmpVector)
+{
+
+}
+
 void fillVector(vector <int>& tmpVector, const int &size)
 {
 	for (int i = 0; i < size; ++i) tmpVector.push_back(rand() % 100);
@@ -10,8 +26,11 @@ void fillVector(vector <int>& tmpVector, const int &size)
 int main()
 {
 	srand(time(NULL));
-	const int size = 10000;
-	vector <int> tmpVector;
-	fillVector(tmpVector, size);
+	const int size = 10;
+	vector <int> tmpVector1, tmpVector2;
+	fillVector(tmpVector1, size);
+	fillVector(tmpVector2, size);
+	sortInsert(tmpVector1);
+	sortInsert(tmpVector2);
 	return 0;
 }
